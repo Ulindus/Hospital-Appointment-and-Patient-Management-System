71106c3cf09e7f48a04cbb97b4fae03f9bd64891
Ulindus <ulindusathsara@gmail.com>
2026-01-12 00:25:54 +0530

    - Redesigned Patient Dashboard with card-based layout - Centered and styled Book Appointment and My Appointments pages

---
+++ Hospital Appointment and Patient Management System/Migrations/20260111150211_ClearOldData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital_Appointment_and_Patient_Management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClearOldData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Appointments");
+            migrationBuilder.Sql("DELETE FROM DoctorSchedules");
+            migrationBuilder.Sql("DELETE FROM Patients");
+            migrationBuilder.Sql("DELETE FROM Doctors");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
